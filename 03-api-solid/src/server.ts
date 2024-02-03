5e4b944a18c033fabed25a7b5fb67cde501f907b
Aislan Dias <aislandias@outlook.com>
2024-02-03 13:30:23 -0300

    [chore]: final project setup

--- 03-api-solid/src/server.ts
+++ 03-api-solid/src/server.ts
@@ -1,9 +1,12 @@
-import { app } from "./app"
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
+app
+  .listen({
     // This host can solve some problems when integrating with front end
     host: '0.0.0.0',
-    port: 3333
-}).then(() => {
+    port: env.PORT,
+  })
+  .then(() => {
     console.log('HTTP Server Running')
-})
+  })
