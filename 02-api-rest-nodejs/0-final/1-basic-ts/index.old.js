d68a4c9bab1cced91e99edf8bd4adda717d07416
Aislan Dias <aislandias@outlook.com>
2024-02-04 14:32:20 -0300

    [clean]: remove unused build files

--- 02-api-rest-nodejs/0-final/1-basic-ts/index.old.js
+++
@@ -1,9 +0,0 @@
-// ?:   required
-// :    not required
-
-function calculateAgeOfUser( user ){
-    return new Date().getFullYear() - user.birthYear
-}
-
-calculateAgeOfUser('Diego')
-calculateAgeOfUser()
