76e790a93bfdc681085c5f48d2248f7966f38ba1
Aislan Dias <aislandias@outlook.com>
2024-03-14 17:00:07 -0300

    [feat] getters and setters on entities

--- 04-clean-ddd/src/domain/entities/answer.ts
+++ 04-clean-ddd/src/domain/entities/answer.ts
@@ -10,9 +10,41 @@ interface AnswerProps {
   updatedAt?: Date;
 }
 
+// Only create setters when you need them, otherwise don't
+// Getters can be useful to implement business logic inside them, or to get
+// specific information
 export class Answer extends Entity<AnswerProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
   get content() {
     return this.props.content;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
   }
 
   static create(
