0d698dd567f15e134b9137390415726bd0edf133
Aislan Dias <aislandias@outlook.com>
2024-03-14 12:37:59 -0300

    [refactor] decoupled project

---
+++ 06-nest-clean-desacoplando/src/infra/database/database.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachment-repository'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+
+// To make PrismaService available for all modules that import this module
+// Use exports
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: QuestionsRepository,
+      useClass: PrismaQuestionsRepository,
+    },
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    QuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+})
+export class DatabaseModule {}
