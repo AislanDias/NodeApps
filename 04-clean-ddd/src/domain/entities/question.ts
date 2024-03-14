5d03454cc28f6b85924f0a13a7499a918f0fea98
Aislan Dias <aislandias@outlook.com>
2024-03-14 15:04:50 -0300

    [feat] slug object-value

--- 04-clean-ddd/src/domain/entities/question.ts
+++ 04-clean-ddd/src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
   title: string;
   content: string;
+  slug: Slug;
   authorId: string;
 }
 
@@ -10,12 +12,22 @@ export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public slug: Slug;
   public authorId: string;
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
     this.id = id ?? randomUUID();
   }
 }
+
+// new Question({
+//   slug: Slug.createFromText('An example title')
+// })
+
+// new Question({
+//   slug: Slug('an-example-title')
+// })
