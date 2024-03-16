6e63e89f89ea1fc2ad500c59048ed55739f42919
Aislan Dias <aislandias@outlook.com>
2024-03-16 10:16:24 -0300

    [feat] question-comment, answer-comment

--- 04-clean-ddd/src/domain/forum/enterprise/entities/answer-comment.ts
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,43 +1,17 @@
-import { Entity } from '@/core/entities/entity'
-
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface AnswerCommentProps {
-  authorId: UniqueEntityId
+export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
 // Only create setters when you need them, otherwise don't
 // Getters can be useful to implement business logic inside them, or to get
 // specific information
-export class AnswerComment extends Entity<AnswerCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+export class AnswerComment extends Comment<AnswerCommentProps> {
+  get answerId() {
+    return this.props.answerId
   }
 
   static create(
