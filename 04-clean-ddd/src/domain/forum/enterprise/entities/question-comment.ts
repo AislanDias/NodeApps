209e42a7a2c1bf9d80274a10b3b5dce361d6dea3
Aislan Dias <aislandias@outlook.com>
2024-03-16 09:09:28 -0300

    [feat-test] fetch-question-answers + tests, add answer-comment, question-comment entitites

---
+++ 04-clean-ddd/src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,56 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+export interface QuestionCommentProps {
+  authorId: UniqueEntityId
+  questionId: UniqueEntityId
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+// Only create setters when you need them, otherwise don't
+// Getters can be useful to implement business logic inside them, or to get
+// specific information
+export class QuestionComment extends Entity<QuestionCommentProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  static create(
+    props: Optional<QuestionCommentProps, 'createdAt'>,
+    id?: UniqueEntityId,
+  ) {
+    const questionComment = new QuestionComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return questionComment
+  }
+}
