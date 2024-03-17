33b2d83ddc49ef218fdd0a5ab84fbbc8d682d163
Aislan Dias <aislandias@outlook.com>
2024-03-17 07:50:53 -0300

    [feat] aggregate lists in question and answers, add attachment entity

--- 04-clean-ddd/src/core/entities/entity.ts
+++ 04-clean-ddd/src/core/entities/entity.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityId } from './unique-entity-id'
 
-export class Entity<Props> {
+export abstract class Entity<Props> {
   // Private Entity
   private _id: UniqueEntityId
   protected props: Props
