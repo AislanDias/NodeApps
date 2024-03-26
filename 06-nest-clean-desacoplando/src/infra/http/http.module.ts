8116f16b818f01efe672f00aae199d1452b16510
Aislan Dias <aislandias@outlook.com>
2024-03-26 10:31:09 -0300

    [feat] delete question controller

--- 06-nest-clean-desacoplando/src/infra/http/http.module.ts
+++ 06-nest-clean-desacoplando/src/infra/http/http.module.ts
@@ -13,6 +13,8 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { EditQuestionController } from './controllers/edit-question-controller'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
+import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
 
 // We need to define CreateQuestionUseCase on providers because we
 // have a controller that depends on it
@@ -25,6 +27,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
     FetchRecentQuestionsController,
     GetQuestionBySlugController,
     EditQuestionController,
+    DeleteQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -33,6 +36,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
     AuthenticateStudentUseCase,
     GetQuestionBySlugUseCase,
     EditQuestionUseCase,
+    DeleteQuestionUseCase,
   ],
 })
 export class HttpModule { }
