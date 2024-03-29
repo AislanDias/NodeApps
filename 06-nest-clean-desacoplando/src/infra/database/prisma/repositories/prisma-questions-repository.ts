cec13b374c9c6b5f17bbe08c719cac78014a2a00
Aislan Dias <aislandias@outlook.com>
2024-03-29 14:28:52 -0300

    [feat] prisma-questions-repository, best answer domain events functionality, e2e-setup disable evetnts by default

--- 06-nest-clean-desacoplando/src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ 06-nest-clean-desacoplando/src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -7,6 +7,7 @@ import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
 import { PrismaQuestionDetailsMapper } from '../mappers/prisma-question-details-mapper'
+import { DomainEvents } from '@/core/events/domain-events'
 
 // Everything we send to another class contructor, we need to make it Injectable
 @Injectable()
@@ -87,6 +88,8 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     await this.questionAttachmentRepository.createMany(
       question.attachments.getItems(),
     )
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async save(question: Question): Promise<void> {
@@ -106,6 +109,8 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
         question.attachments.getRemovedItems(),
       ),
     ])
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete(question: Question): Promise<void> {
